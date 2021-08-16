9f0e3f9c870c80219666e20a171f4f8d04baa957
Loakesh I <loakesh.indiran@wowlabz.com>
2021-08-16 11:36:08 +0530

    Add: tasking_frontend auth feature testing Implemented:
    Because:

---
+++ src/View/Modules/Authorization/LoginFormFormik.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import {
+    ErrorMessage,
+    Field,
+    Form as FormikForm,
+    Formik,
+    useField,
+} from "formik";
+import * as Yup from "yup";
+import {
+    Card,
+    Form,
+    FormControl,
+    Button,
+    Col,
+    Row,
+    Badge,
+    Modal,
+    Spinner,
+} from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import FormErrorMessage from "./FormErrorMessage";
+import * as constants from "./constants";
+import { userSignIn } from "./actionCreators";
+import apiHelpers from "../../../Utilities/axiosHelpers";
+
+let initialValues = {
+    userName: "",
+    password: "",
+};
+
+const validationSchema = Yup.object({
+    userName: Yup.string().email("Invalid email format").required("Required!"),
+    password: Yup.string()
+        .min(6, "Minimum 6 characters required!")
+        .required("Required!"),
+});
+
+const FixedCardHeight = {
+    height: "600px",
+};
+
+const LoginFormFormik = () => {
+    const history = useHistory();
+    const isLoggedIn = useSelector((state) => state.authenticationReducer.isLoggedIn);
+
+    const handleFormSubmit = async (data) => {
+        let loginFormData = new FormData();
+        loginFormData.append("username", data.userName);
+        loginFormData.append("password", data.password);
+        console.log(loginFormData.get("username"));
+        console.log(loginFormData.get("password"));
+        // let test = await apiHelpers.get(constants.AUTH_BASE_URL);
+        // console.log(test);
+        // userSignIn(loginFormData);
+        if (isLoggedIn) {
+            history.push("/dashboard");
+        }
+    };
+
+    return (
+        <>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={async (data, { setSubmitting, resetForm }) => {
+                    setSubmitting(true);
+                    handleFormSubmit(data);
+                    setSubmitting(false);
+                    resetForm();
+                }}
+            >
+                {({
+                    values,
+                    errors,
+                    isSubmitting,
+                    resetForm,
+                    handleForm,
+                    handleSubmit,
+                }) => (
+                    <FormikForm>
+                        <Card style={{ ...FixedCardHeight }}>
+                            <Card.Body>
+                                <FormLabelAndInput
+                                    placeholder="username"
+                                    name="userName"
+                                    type="email"
+                                    label="User Name"
+                                    helperText={"Enter registered EmailId"}
+                                    isDisabled={false}
+                                />
+                                <FormLabelAndInput
+                                    placeholder="password"
+                                    name="password"
+                                    type="password"
+                                    label="Password"
+                                    helperText={""}
+                                    isDisabled={false}
+                                />
+                            </Card.Body>
+                            <Card.Footer className="d-flex justify-content-center aligin-items-center">
+                                <Button
+                                    variant="dark"
+                                    type="submit"
+                                    name={`Sign In`}
+                                >
+                                    <b>{`Sign In`}</b>
+                                </Button>
+                            </Card.Footer>
+                        </Card>
+                    </FormikForm>
+                )}
+            </Formik>
+        </>
+    );
+};
+
+const FormLabelAndInput = ({ label, helperText, isDisabled, ...props }) => {
+    const [field, meta] = useField(props);
+
+    return (
+        <Form.Group style={{ minHeight: "12vh" }}>
+            <Form.Label className="publish-form-label mtl-5">
+                {label}
+            </Form.Label>
+            {!isDisabled && (
+                <ErrorMessage
+                    name={field.name}
+                    component={FormErrorMessage}
+                ></ErrorMessage>
+            )}
+            <Field
+                {...field}
+                {...props}
+                as={FormControl}
+                autoComplete="off"
+                disabled={isDisabled}
+                className="p-2"
+            ></Field>
+            <small>{helperText}</small>
+        </Form.Group>
+    );
+};
+
+export default LoginFormFormik;
---
+++ src/View/Modules/Authorization/constants.js
@@ -0,0 +1,16 @@
+export const USERTYPE = {
+    CUSTOMER: "Customer",
+    WORKER: "Worker" 
+}
+
+export const SIGN_IN = "SIGN_IN"
+export const SIGN_UP = "SIGN_UP"
+export const INITIATING_API_CALL = "INITIATING_API_CALL"
+export const API_CALL_ERROR = "API_CALL_ERROR"
+export const API_CALL_SUCCESS = "API_CALL_SUCCESS"
+
+export const AUTH_BASE_URL = "http://localhost:3001"
+export const AUTH_END_POINTS = {
+    signUp: "/auth/sign-up",
+    signIn: "/auth/sign-in"
+}
